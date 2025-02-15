11d7d8df0e6d3476931a4fa991057dd23fb2a58d
Yang Zhou <zz1999999999@outlook.com>
2025-02-15 22:23:58 +0800

    Add files via upload
    First commit.

---
+++ evaluation/evaluation.py
@@ -0,0 +1,89 @@
+import math
+
+import numpy as np
+import torch
+from sklearn.metrics import average_precision_score, roc_auc_score
+
+
+def eval_edge_prediction(model, negative_edge_sampler, data, n_neighbors, batch_size=200):
+  # Ensures the random sampler uses a seed for evaluation (i.e. we sample always the same
+  # negatives for validation / test set)
+  assert negative_edge_sampler.seed is not None
+  negative_edge_sampler.reset_random_state()
+
+  val_ap, val_auc = [], []
+  with torch.no_grad():
+    model = model.eval()
+    # While usually the test batch size is as big as it fits in memory, here we keep it the same
+    # size as the training batch size, since it allows the memory to be updated more frequently,
+    # and later test batches to access information from interactions in previous test batches
+    # through the memory
+    TEST_BATCH_SIZE = batch_size
+    num_test_instance = len(data.sources)
+    num_test_batch = math.ceil(num_test_instance / TEST_BATCH_SIZE)
+    norm_list = []
+
+    for k in range(num_test_batch):
+      s_idx = k * TEST_BATCH_SIZE
+      e_idx = min(num_test_instance, s_idx + TEST_BATCH_SIZE)
+      sources_batch = data.sources[s_idx:e_idx]
+      destinations_batch = data.destinations[s_idx:e_idx]
+      timestamps_batch = data.timestamps[s_idx:e_idx]
+      edge_idxs_batch = data.edge_idxs[s_idx: e_idx]
+
+      size = len(sources_batch)
+      _, negative_samples = negative_edge_sampler.sample(size)
+
+      pos_prob, neg_prob, norm = model.compute_edge_probabilities(sources_batch, destinations_batch,
+                                                            negative_samples, timestamps_batch,
+                                                            edge_idxs_batch, n_neighbors)
+
+      pred_score = np.concatenate([(pos_prob).cpu().numpy(), (neg_prob).cpu().numpy()])
+      true_label = np.concatenate([np.ones(size), np.zeros(size)])
+      true_labels = np.expand_dims(true_label, axis=-1)
+      pred_labels = np.concatenate([pos_prob.cpu().numpy() >= 0.5, neg_prob.cpu().numpy() >= 0.5])
+      accuracy = np.mean(pred_labels == true_labels)
+      # print(f"Accuracy: {accuracy:.4f}")
+
+      val_ap.append(average_precision_score(true_label, pred_score))
+      val_auc.append(roc_auc_score(true_label, pred_score))
+      norm_list.append(norm)
+
+  return np.mean(val_ap), np.mean(val_auc), accuracy
+  # return np.mean(val_ap), np.mean(val_auc),
+
+
+def eval_node_classification(tgn, decoder, data, edge_idxs, batch_size, n_neighbors):
+  pred_prob = np.zeros(len(data.sources))
+  num_instance = len(data.sources)
+  num_batch = math.ceil(num_instance / batch_size)
+
+  with torch.no_grad():
+    decoder.eval()
+    tgn.eval()
+    for k in range(num_batch):
+      s_idx = k * batch_size
+      e_idx = min(num_instance, s_idx + batch_size)
+
+      sources_batch = data.sources[s_idx: e_idx]
+      destinations_batch = data.destinations[s_idx: e_idx]
+      timestamps_batch = data.timestamps[s_idx:e_idx]
+      edge_idxs_batch = edge_idxs[s_idx: e_idx]
+
+      source_embedding, destination_embedding, _, regularization_norm = tgn.compute_temporal_embeddings(sources_batch,
+                                                                                   destinations_batch,
+                                                                                   destinations_batch,
+                                                                                   timestamps_batch,
+                                                                                   edge_idxs_batch,
+                                                                                   n_neighbors)
+      pred_prob_batch = decoder(source_embedding).sigmoid()
+      pred_prob[s_idx: e_idx] = pred_prob_batch.cpu().numpy()
+  positive_indices = np.where(data.labels == 1)[0]
+  negative_indices = np.where(data.labels == 0)[0]
+  pred_labels = pred_prob >= 0.5
+  positive_accuracy = np.mean(pred_labels[positive_indices] == data.labels[positive_indices])
+  negative_accuracy = np.mean(pred_labels[negative_indices] == data.labels[negative_indices])
+  accuracy = (positive_accuracy+negative_accuracy)/2
+  auc_roc = roc_auc_score(data.labels, pred_prob)
+  ap = average_precision_score(data.labels, pred_prob)
+  return ap ,auc_roc ,accuracy
